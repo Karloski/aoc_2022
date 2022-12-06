036e5b653681a69db85d1aea466a7e90a4b53594
carlwithak <me@carlwithak.me.uk>
2022-12-06 13:36:21 +0000

    Day 6 Part 2

---
+++ day6/part2.ts
@@ -0,0 +1,42 @@
+import { createReadStream } from 'fs'
+import { createInterface } from 'node:readline'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const reader = createInterface(createReadStream(__dirname+'/input.txt'))
+
+const start = Date.now()
+
+let characters = 0
+let map = {}
+
+reader.on('line', (line) => {
+  for (const char of line) {
+    characters++
+
+    if (char in map) {
+      for (const key of Object.keys(map)) {
+        delete map[key]
+
+        if (key === char) {
+          break
+        }
+      }
+    }
+
+    map[char] = true
+
+    if (Object.keys(map).length >= 14) {
+      return
+    }
+  }
+})
+
+reader.on('close', () => {
+  console.log('Characters processed', characters)
+
+  console.log('Process took', Date.now() - start, 'ms')
+})
